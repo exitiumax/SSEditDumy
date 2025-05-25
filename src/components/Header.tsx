a4d5d456f371dddc5db525744e7648f6bf7cc8ec
exitiumax <maxjg99@gmail.com>
2025-05-25 14:55:30 -0500

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -103,7 +103,7 @@ const Header: React.FC = () => {
     { name: 'Blog', path: '/resources/blog' },
     { name: 'Events', path: '/resources/events' },
   ]
-}
+},
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
